e99dd8618616067f63e6e32cc4d0ddbc72d515c8
rickard uttermalm <rickard.uttermalm@gmail.com>
2019-11-13 11:39:26 +0100

    missed one small thing

--- AlmLabb/Business/TransactionResult.cs
+++ AlmLabb/Business/TransactionResult.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace AlmLabb.Models.ViewModels
+namespace AlmLabb.Business
 {
     public class TransactionResult
     {
--- AlmLabb/Models/ViewModels/TransactionViewModel.cs
+++ AlmLabb/Models/ViewModels/TransactionViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AlmLabb.Business;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
